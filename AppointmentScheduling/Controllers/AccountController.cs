97d221bcce16fa3b3b0e4b3303206b468e5b51e9
Akhilesh <360akhileshsingh.it@gmail.com>
2023-11-03 20:27:38 +0530

    Create a Login View.

---
+++ AppointmentScheduling/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentScheduling.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+                
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- AppointmentScheduling/Models/ViewModels/LoginViewModel.cs
+++ AppointmentScheduling/Models/ViewModels/LoginViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace AppointmentScheduling.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppointmentScheduling.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Remember me?")]
+        public bool RememberMe { get; set; }
     }
 }
