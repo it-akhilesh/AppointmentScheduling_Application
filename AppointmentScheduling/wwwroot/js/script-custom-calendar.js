6cdcdaee3b28e3c3a27546d30aab91e8eb5f83a8
Akhilesh <360akhileshsingh.it@gmail.com>
2023-11-24 23:17:03 +0530

    Validation.

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -52,7 +52,40 @@ function onSubmitForm() {
     };
 
     $.ajax({
-        url: routeURL + '/api/Appointment/SaveCalendarDate',
+        url: routeURL + '/api/Appointment/SaveCalendarData',
         type: 'POST',
-    })
+        data: JSON.stringify(requestData),
+        contentType: 'application/json',
+        success: function (response) {
+            if (response.status === 1 || response.status === 2) {
+                $.notify(response.message, "success");
+                onCloseModal();
+            }
+            else {
+                $.notify(response.message, "error");
+
+            }
+        },
+        error: function (xhr) {
+            $.notify("Error", "error");
+        }
+    });
+}
+function checkValidation() {
+    var isValid = true;
+    if ($("#title").val() === undefined || $("#title").val() === "") {
+        isValid = false;
+        $("#title").addClass('error');
+    }
+    else {
+        $("#title").removeClass('error');
+    }
+
+    if ($("#appointmentDate").val() === undefined || $("#appointmentDate").val() === "") {
+        isValid = false;
+        $("#appointmentDate").addClass('error');
+    }
+    else {
+        $("#appointmentDate").removeClass('error');
+    }
 }
