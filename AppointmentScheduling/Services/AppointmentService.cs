25e78e8aef1f7d5e197affa0e76092ef2e823a23
Akhilesh <360akhileshsingh.it@gmail.com>
2023-11-27 22:54:34 +0530

    Retieve Doctor and Patients appointment  in Appointment Service.

--- AppointmentScheduling/Services/AppointmentService.cs
+++ AppointmentScheduling/Services/AppointmentService.cs
@@ -46,6 +46,21 @@ namespace AppointmentScheduling.Services
             
         }
 
+        public List<AppointmentVM> DoctorsEventsById(string doctorId)
+        {
+            return _db.Appointments.Where(x => x.DoctorId == doctorId).ToList().Select(c => new AppointmentVM()
+            {
+                Id = c.Id,
+                Description = c.Description,
+                StartDate = c.StartDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                EndDate = c.EndDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                Title = c.Title,
+                Duriation=c.Duriation,
+                IsDoctorApproved  = c.IsDoctorApproved
+
+            }).ToList();
+        }
+
         public List<DoctorVM> GetDoctorList()
         {
             var doctors = (from user in _db.Users
@@ -73,6 +88,21 @@ namespace AppointmentScheduling.Services
                            ).ToList();
             return patients;
 
+        }
+
+        public List<AppointmentVM> PatientsEventsById(string patientId)
+        {
+            return _db.Appointments.Where(x => x.PatientId == patientId).ToList().Select(c => new AppointmentVM()
+            {
+                Id = c.Id,
+                Description = c.Description,
+                StartDate = c.StartDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                EndDate = c.EndDate.ToString("yyyy-MM-dd HH:mm:ss"),
+                Title = c.Title,
+                Duriation = c.Duriation,
+                IsDoctorApproved = c.IsDoctorApproved
+
+            }).ToList();
         }
     }
 }
