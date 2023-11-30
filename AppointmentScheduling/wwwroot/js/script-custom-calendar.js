e95fbad5b99ce5f7bb5764171bc42b36254c703d
Akhilesh <360akhileshsingh.it@gmail.com>
2023-11-30 22:11:56 +0530

    Calendar values formatting.

--- AppointmentScheduling/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -88,6 +88,14 @@ function onShowModal(obj, isEventDetail) {
     $("#appointmentInput").modal("show");
 }
 function onCloseModal() {
+    $("appointmentForm")[0].reset();
+    $("#id").val(0);
+    $("#title").val('');
+    $("#description").val('');
+    $("#appointmentDate").val('');
+    $("#duration").val('');
+   
+    $("#patientId").val('');
     $("#appointmentInput").modal("hide");
 }
 function onSubmitForm() {
@@ -109,6 +117,7 @@ function onSubmitForm() {
             contentType: 'application/json',
             success: function (response) {
                 if (response.status === 1 || response.status === 2) {
+                    calendar.refetchEvents();
                     $.notify(response.message, "success");
                     onCloseModal();
                 }
